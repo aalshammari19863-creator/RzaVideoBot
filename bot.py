d4747197decfe5b40b61ea78e4831f6fce14b008
aalshammari19863-creator <aalshammari19863@gmail.com>
2026-06-17 03:35:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,31 @@
+import os
 
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes
+)
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "👋 أهلاً بك في RzaVideoBot\n\n"
+        "البوت يعمل بنجاح."
+    )
+
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot Started")
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
